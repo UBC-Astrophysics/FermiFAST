ab01ffcbb12451cccf3bf3e6a18109d4cd7197a1
Jeremy Heyl <heyl@phas.ubc.ca>
2016-03-23 09:45:59 -0700

    added new plots

---
+++ results/cum0p1GeV.sh
@@ -0,0 +1,5 @@
+ctioga2 --xfact  57.297469362 --xlog  --name cum0p1GeV cum_0.1GeV_1GeV_hires_uniq_match@2:1 \
+	cum_0.1GeV_1GeV_hires_uniq_match_flipped@2:1 \
+	cum_all_ts_data_0.1GeV_fermi_match@2:1 \
+	cum_all_ts_data_0.1GeV_fermi_match_flipped@2:1 \
+	-x 'Distance to Nearest 1FF/3FGL Source [degrees]' -y 'Cumulative Number' 
