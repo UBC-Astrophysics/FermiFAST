6f31a6e831294efe8e77f002130ff89de51681fc
Jeremy Heyl <heyl@phas.ubc.ca>
2016-12-06 22:49:35 -0800

    r2

--- results/cumff.sh
+++ results/cumff.sh
@@ -4,7 +4,8 @@ ctioga2 --mark --name cumff \
 	cum_all_ts_data_1GeV_hires_uniq_fermi_match_flipped@'log10($2*45/atan(1.0)):$1' \
 	cum_all_ts_data_1GeV_hires_uniq_fermi_match_flipped@'log10($2*45/atan(1.0)):($1)*(800.0-765.0)/800.0+765.0' \
 	--math /xrange -4:1 \
+	-c Red --line-style dashes \
 	'325*(1-exp(-10**(2*x)/2/0.005/0.005))+345*(1-exp(-10**(2*x)/2/0.018/0.018))+95*(1-exp(-10**(2*x)/2/0.11/0.11))+35.0*(1-exp(-10**(2*x)/2/1.5/1.5))' \
-	'800*(1-exp(-10**(2*x)/2/1.5/1.5))' \
+	-c Green --line-style dashes '800*(1-exp(-10**(2*x)/2/1.5/1.5))' \
 	-x 'Distance to Nearest Fermi 3FGL Source [Degrees]' \
 	-y 'Cumulative Number' --xrange -4:1  --xlog --yrange 0:800
--- results/cumff_hib.sh
+++ results/cumff_hib.sh
@@ -3,11 +3,11 @@ ctioga2 --mark --name cumff_hib \
 	cum_all_ts_data_1GeV_hires_uniq_fermi_match_hib@'log10($2*45/atan(1.0)):$1' \
 	cum_all_ts_data_1GeV_hires_uniq_fermi_match_flipped_hib@'log10($2*45/atan(1.0)):$1' \
 	--math /xrange -4:1 \
-	-c Cyan \
+	-c Red --line-style dashes \
 	'300*(1-exp(-10**(2*x)/2/0.005/0.005))+230*(1-exp(-10**(2*x)/2/0.015/0.015))+42*(1-exp(-10**(2*x)/2/0.05/0.05))+8*(1-exp(-10**(2*x)/2/1.5/1.5))' \
-	-c Magenta \
+	-c Green --line-style dashes \
 	'580*(1-exp(-10**(2*x)/2/1.5/1.5))' \
-	-c Blue --text \
+	-c Blue --line-style solid --text \
 	cum_all_ts_data_1GeV_hires_uniq_fermi_match_flipped_hib@'log10($2*45/atan(1.0)):$1*(580.0-572.0)/580.0+572.0' \
 	-x 'Distance to Nearest Fermi 3FGL Source [Degrees]' \
 	-y 'Cumulative Number' --xrange -4:1  --xlog --yrange 0:650
